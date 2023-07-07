ac4679dd44995b9c2682707cc87840b8ccaf8844
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-07-07 14:51:58 -0300

    Can't believe I forgot init.lua

---
+++ init.lua
@@ -0,0 +1,33 @@
+require("init")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = { import = 'plugin-specs' },
+  change_detection = { notify = false },
+  performance = {
+    cache = { enabled = true },
+    disabled_plugins = {
+      "gzip",
+      "matchit",
+      "matchparen",
+      "tarPlugin",
+      "tohtml",
+      "zipPlugin",
+    }
+  }
+})
+
