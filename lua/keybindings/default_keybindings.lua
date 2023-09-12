70cf9ba6755708dcc11d2aad2486fe267c7f00da
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-09-12 07:55:39 -0300

    Added temporary patch for keybindings to work

--- lua/keybindings/default_keybindings.lua
+++ lua/keybindings/default_keybindings.lua
@@ -1,57 +1,11 @@
-function MapKey(binding)
-  if (type(binding) ~= 'table'
-        or not binding.key
-        or not binding.does
-        or type(binding.key) ~= 'string'
-        or type(binding.does) ~= 'string'
-      ) then
-    return false
-  end
-
-  if not binding.in_mode or type(binding.in_mode) ~= 'string' then
-    binding.in_mode = 'n'
-  end
-
-  if not binding.with_options or type(binding.with_options) ~= 'table' then
-    binding.with_options = {
-      noremap = true,
-      silent = true
-    }
-  end
-
-  vim.api.nvim_set_keymap(
-    binding.in_mode,
-    binding.key,
-    binding.does,
-    binding.with_options
-  )
-end
-
 --Remap space as leader key
 ---|bind('', '<Space>', '<Nop>')
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Modes
---   normal_mode = 'n',
---   insert_mode = 'i',
---   visual_mode = 'v',
---   visual_block_mode = 'x',
---   term_mode = 't',
---   command_mode = 'c',
-
--- Normal --
--- Better window navigation
-
-
--- bind('n', '<leader>e', ':Lex 30<cr>')
-
-
-local ComeHere
-
-
-
-
+--                | N | Normal           | I | Insert
+--                | V | Visual           | X | Vblock
+--                | C | Command          | T | Terminal
 
 -- Resize with arrows
 MapKey({ key = '<C-Up>', does = ':resize -1<CR>' })
--- lua/keybindings/init.lua
+++ lua/keybindings/init.lua
@@ -1,1 +1,69 @@
+--    |                                     |  Mode Characters Quick Reference
+--    | N | Normal           | I | Insert   |
+--    | V | Visual           | X | Vblock   |
+--    | C | Command          | T | Terminal |
+--    |_____________________________________|
+
+function MapKey(binding)
+  if (type(binding) ~= 'table'
+        or not binding.key
+        or not binding.does
+        or type(binding.key) ~= 'string'
+        or type(binding.does) ~= 'string'
+      ) then
+    return false
+  end
+
+  if not binding.in_mode or type(binding.in_mode) ~= 'string' then
+    binding.in_mode = 'n'
+  end
+
+  if not binding.with_options or type(binding.with_options) ~= 'table' then
+    binding.with_options = {
+      noremap = true,
+      silent = true
+    }
+  end
+
+  vim.api.nvim_set_keymap(
+    binding.in_mode,
+    binding.key,
+    binding.does,
+    binding.with_options
+  )
+
+  -- which_key.register(mappings, {
+  --   prefix = "<leader>l",
+  --   mode = "n",
+  --   buffer = bufnr
+  -- })
+
+  if binding.with_wk and type(binding.with_wk) == 'table' then
+    events.run_after("configured", "which-key", function()
+      local wk_ok, wk = pcall(require, 'which-key')
+
+      if not wk_ok then
+        return 1
+      end
+
+      -- if type(w) 
+
+      if (wk_ok and (type(wk['register']) == 'function')) then
+        local mapping = {
+
+        }
+
+        wk.register(mappings, {
+          prefix = wK_leader,
+          binding.in_mode,
+        })
+      elseif type(wk.register) == 'table' then
+
+      end
+    end)
+  end
+end
+
+
 require 'keybindings.default_keybindings'
+require 'keybindings.lsp_dynamic_keymaps'
