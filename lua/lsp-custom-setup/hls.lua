0616367903d2594e6c9a7a409d6ecf27c9c6822a
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-10-16 15:01:25 -0300

    Updated hls.lua for new haskell-tools versions

--- lua/lsp-custom-setup/hls.lua
+++ lua/lsp-custom-setup/hls.lua
@@ -1,22 +1,28 @@
 local events = require('lib.events')
 
 return function(hls, caps, on_attach)
-  local root_pattern = require("lspconfig.util").root_pattern
+  -- Attempt to use haskell-tools if available.
+  local ht_present, ht = pcall(require, "haskell-tools")
+
+  -- If available, haskell-tools will handle the rest
+  if ht_present then
+    -- Older versions require a call to setup.
+    if type(ht) == 'table' and type(ht['setup']) == 'function' then
+      ht['setup']()
+    end
+
+    return
+  end
+
+  -- If haskell-tools isn't available, go through the manual
+  -- lspconfig setup.
 
   local config = {
     on_attach    = on_attach,
     capabilities = caps,
   }
 
-  local haskell_tools_present, haskell_tools = pcall(require, "haskell-tools")
-
-  if haskell_tools_present then
-    haskell_tools.setup {}
-  elseif hls and type(hls['setup']) == 'function' then
-    hls.setup(config)
-  else
-    vim.notify('Custom setup for LSP received null instance of language server: ' .. hls .. = ' = ' .. type(hls))
-  end
+  hls.setup(config)
 
   return true
 end
