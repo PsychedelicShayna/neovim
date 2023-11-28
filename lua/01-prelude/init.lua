2fa96da82a7ebc00ceec50312c8100248522e4c6
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-11-28 02:54:28 -0300

    Adjuted prelude init, and WIP featurs.

--- lua/01-prelude/init.lua
+++ lua/01-prelude/init.lua
@@ -8,30 +8,44 @@ LL_WARN  = vim.log.levels.WARN  -- 3
 LL_ERROR = vim.log.levels.ERROR -- 4
 LL_OFF   = vim.log.levels.OFF   -- 5
 
-NOP = function() end
+NOP      = function() end
 
 --- Error/Debug Printing ------------------------------------------------------
 --
 
-function eprint(message, level, inspections)
-  if not level then 
+
+-- Setting this to true, will allow the below function to take effect.
+DEBUG_MODE = false
+
+function PrintError(module_name, message, inspections)
+  vim.notify('[' .. module_name .. '] ' .. message)
+end
+
+
+function PrintDbg(message, level, inspections)
+  if not level then
     level = LL_INFO
   end
 
   vim.notify(message, level)
 
   if type(inspections) == "table" then
-    for i, item in inspections do
-      vim.notify('I' .. tostring(i) .. ";" .. type(item) .. ':'  .. vim.inspect(tostring(item)))
+    for index, item in ipairs(inspections) do
+      local item_type = type(item)
+      local item_str = "nil"
+
+      if item_type ~= 'nil' then
+        item_str = vim.inspect(item)
+      end
+
+      vim.notify('I' .. index .. ";" .. item_type .. ':' .. item_str)
     end
   end
 
   return level
 end
 
-
-
---- Export Error Handling System ---------------------------------------------- 
+--- Export Error Handling System ----------------------------------------------
 --
 
 Safety = {}
@@ -41,34 +55,33 @@ local safety_ok, safety = pcall(require, "01-prelude.safety")
 if safety_ok then
   Safety = safety
 else
-  eprint("Prelude failed to import a component of itself: safety.lua", LL_ERROR)
-  eprint("The error was caught via pcall, so the whole config does not crash.", LL_INFO)
-  eprint("The pcall return values... ", LL_DEBUG, {safety_ok, safety})
-  eprint("The Safety global: ", LL_DEBUG, {Safety})
+  PrintDbg("Prelude failed to import a component of itself: safety.lua", LL_ERROR)
+  PrintDbg("The error was caught via pcall, so the whole config does not crash.", LL_INFO)
+  PrintDbg("The pcall return values... ", LL_DEBUG, { safety_ok, safety })
+  PrintDbg("The Safety global: ", LL_DEBUG, { Safety })
 
   Safety = 0xDEADC0DE
 end
 
---- Export Event System ------------------------------------------------------- 
+--- Export Event System -------------------------------------------------------
 --
-
-eprint("Prelude failed to import a component of itself: safety.lua", LL_ERROR)
-eprint("The error was caught via pcall, so the whole config does not crash.", LL_INFO)
 
 Events = {}
 
-local events_ok, events = pcall(require, "events")
+local events_ok, events = pcall(require, "01-prelude.events")
 
 if events_ok then
   Events = events
 else
-  eprint("Prelude failed to import a component of itself: safety.lua", LL_ERROR)
-  eprint("The error was caught via pcall, so the whole config does not crash.", LL_INFO)
-  eprint("The pcall return values... ", LL_DEBUG, {safety_ok, safety})
-  eprint("The Safety global: ", LL_DEBUG, {Safety})
+  PrintDbg(
+    "Prelude failed to import a component of itself [events.lua] inspection: ",
+    LL_ERROR, {
+      events,
+      events_ok
+    })
 
   Events = 0xDEADC0DE
 end
 
---                                                                           
+--
 --- EOF, End of File ----------------------------------------------------------
