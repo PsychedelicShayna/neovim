4bfa1dc8d7e3b38baaea218c327fbda85fc448f0
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-11-28 03:23:52 -0300

    A new plugin to replace nvim-ts-rainbow (currently deactivated)

---
+++ lua/05-plugins/spec/rainbow-delimiters.lua
@@ -0,0 +1,14 @@
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  lazy = true,
+  config = function()
+    -- local rd_ok, rd = pcall(require, "rainbow-delimiters.setup")
+    --
+    -- if not rd_ok then
+    --   PrintDbg("Failed to load rainbow-delimiters", LL_ERROR, { rd_ok, rd })
+    --   return
+    -- end
+    --
+    -- rd.setup()
+  end
+}
