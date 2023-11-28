d39b57c5fac4ab2cdf2c20c18f9b3350eb7257c4
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-11-28 02:54:04 -0300

    Improved event system.

--- lua/01-prelude/events.lua
+++ lua/01-prelude/events.lua
@@ -24,7 +24,9 @@ end
 -------------------------------------------------------------------------------
 
 function M.await_event(opts)
-  if type(opts) ~= 'table' then return nil end
+  if type(opts) ~= 'table' then
+    return nil
+  end
 
   if type(opts['actor']) ~= 'string' or type(opts['event']) ~= 'string' then
     if type(opts['actor']) ~= 'string' then
@@ -57,15 +59,67 @@ function M.await_event(opts)
 
   if type(opts['callback']) ~= 'function' then
     vim.notify('You have to define a callback function in your table when using await_event!', vim.log.levels.ERROR)
-    return nil
+    return id
   end
 
-  if not M.database[id] and opts['ignore_unknown'] then
-    M.database[id] = { times_fired = 1, data = opts.callback() }
-    return nil
+  -- Storing Repetitive Conditional Expressions --
+  ------------------------------------------------
+
+  -- Table under this event's ID must be present.
+  local event_exists = type(M.database[id]) == 'table' or false
+  -----------------------------------------
+
+
+  local data_exists = (event_exists
+    and type(M.database[id]['data']) == 'table')
+  ---------------------------------------------
+  -- Does the event table have a data table? --
+  ---------------------------------------------
+
+
+  local queue_exists = (type(M.awaiting) == 'table'
+    and type(M.awaiting[id]) == 'table')
+  --                  ------------------------------------
+  --
+  local others_awaiting = (queue_exists and #M.awaiting[id] > 0)
+  --                        --------------------------------------
+  --                        -- Is there at least one calback in --
+  --                        -- the awaiting queue?              --
+  --                        ------------------------------------
+
+
+  local data = {}
+
+  if data_exists then
+    data = M.database[id]['data']
   end
 
-  if not M.database[id] then
+  -- If true, don't schedule the event, just bypass and invoke the callback
+  -- immediatlely when this funciton is called.
+
+  local dont_wait = (
+    opts['or_never'] and (
+    -- Table has never been defined.
+      (not event_exists)
+
+      -- Table has been defined, but times_fired is 0.
+      or (type(M.database[id].times_fired) == 'number'
+        and M.database[id].times_fired == 0)
+    )
+  ) or (
+    opts['or_previously'] and (
+    -- Table is defined, has a times_fired int, and fired at least once.
+      (type(M.database[id]) == 'table' and
+        type(M.database[id].times_fired) == 'number' and
+        M.database[id].times_fired >= 1)
+    )
+  )
+
+  if dont_wait then
+    return opts['callback']()
+  end
+
+  if not event_exists then
     M.database[id] = { times_fired = 0, data = {} }
   end
 
@@ -77,6 +131,67 @@ function M.await_event(opts)
   return id
 end
 
+-------------------------------------------------------------------------------
+--
+-- When a responder attached to an event (callback), is called because an actor
+-- fired an event (transmitter), something interesting can happen.
+--
+-- The event has an ID, which can be used to look-up a table of arbitrary data
+-- attached to the event. This table is passed to the callback as an argument,
+-- containing previous state.
+--
+-- The return value of the callback, if a table or function returning a table,
+-- will replace the previous table associated with that event ID; an update.
+--
+-- Why is that interesting? Because when the actor fires the event, it too can
+-- update the table by providing a table *or a function*, accepting a table
+-- as an argument. The existing table will be passed to it, and just like the
+-- callback, it can return a new table used to replace the existing one.
+--
+-- The key point: the actor's function that receives the previous responders'
+-- table, is called BEFORE any responder scheduled for the event being fired
+-- at the moment is called.
+--
+-- In other words, the actor can update the data based on the previous update
+-- by a receiver, then update it before it reaches the current receiver, and
+-- the receiver can then update it for the future actor to receive, again,
+-- updating it and passing it to the future receiver.
+--
+--                             .                 .
+--                        .         .       .        .
+--
+--                    @     ||__________*__________||     .
+--       Transmitter        |-----------------------|        Ping
+--  ~~~~~~~~~~~~~~~~~~~~~~~~||  Yes. Pong. Ping.   ||~~~~~~~~~~~~~~~~~~~~~~
+--          Pong            ||                     ||       Receiver
+--                                 See Below.
+--
+--
+--
+-------------------------------  Single Receiver  -----------------------------
+--
+--
+--   |v----------------------<|
+--   |> = {} => T = {} => R =^|
+--   ---------!---------!------
+--            !         !  Present R receives {} assigned by past T
+--            !         !   .. R assigns {} for future T
+--            !
+--            !  Present T receives {}, assigned by past R
+--            !   .. assigns {} for future R
+--
+--
+-----------------------------  Multiple Receivers  ----------------------------
+--
+--     database
+--     {
+--       [event_id] ={R3} <----{=}      {v} <----{=}
+--     }          ^        3rd>|R|  2nd>|R|  1st>|R|
+--                ^            {^} <----{=}      {^}<-------{t}
+--                ^                                          ^
+--                ? > ----------------------- > T > fn({R3}) ^
+--
+--
 -------------------------------------------------------------------------------
 
 function M.fire_event(opts)
@@ -98,12 +213,12 @@ function M.fire_event(opts)
   if type(M.database[id]) == "table" then
     M.database[id].times_fired = M.database[id].times_fired + 1
 
-    local set_data_t = type(opts['set_data'])
+    local set_data_t = type(opts['data'])
 
     if set_data_t == 'table' then
-      M.database[id].data = opts['set_data']
+      M.database[id].data = opts['data']
     elseif set_data_t == 'function' then
-      local new_data = opts['set_data'](M.database[id].data)
+      local new_data = opts['data'](M.database[id].data)
       if type(new_data) == 'table' then M.database[id] = new_data end
     end
   else
@@ -142,8 +257,10 @@ function M.fire_event(opts)
 
   -- Removes Expired Callbacks -------------------------------------
   for _, index in ipairs(expired) do
-    table.remove(M.await_event[id], index)
+    table.remove(M.awaiting[id], index)
   end
 end
+
+Events = M
 
 return M
