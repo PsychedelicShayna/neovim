6832f1c22877c00666011dffd8806036364f7b0f
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-12-01 19:16:05 -0300

    Added plugin: glow

---
+++ lua/05-plugins/spec/glow.lua
@@ -0,0 +1,25 @@
+return {
+  "ellisonleao/glow.nvim",
+  cmd = "Glow",
+  keys = "gL",
+  config = function()
+    Safe.import_then("glow", function(glow)
+
+      Safe.try(function()
+        glow.setup {
+          glow_path       = "/usr/bin/glow",
+          install_path    = "~/.local/state/nvim/dep",
+          border          = "single",
+          -- style        = "dark|light",  -- Determined automatically if unset.
+          -- pager        = false,
+          -- width        = 80,
+          -- height       = 100,
+          -- height_ratio = 0.7,           -- Maximum height relative to Neovim.
+        }
+      end)
+          -- width_ratio  = 0.7,           -- Maximum width relative to Neovim.
+
+      MapKey { in_mode = "n", key = "gL", does = ":Glow<CR>" }
+    end)
+  end
+}
