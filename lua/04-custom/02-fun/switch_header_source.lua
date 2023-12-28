51743645e6e2e6f89102572c4de4a5e1d9559d9b
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2023-12-28 20:43:01 -0300

    Custom header/source switch because clangd-extensions' is scuffed.
    The problem with clangd-extensions' implementation is that it only looks for the
    matching header in the same directory as the source file, and has no fallback
    to look through open buffers instead if a match isn't found.
    
    This is annoying when you structure your C++ project in a way that separates
    headers and source files into separate directories.
    
    Currently not operatoinal, just scaffolding for the most part. Work in progress.
    This implementation should (configurably) priioritize the current folder by
    default, and fall back to looking through opened buffers, by default anyway.

---
+++ lua/04-custom/02-fun/switch_header_source.lua
@@ -0,0 +1,87 @@
+local function switch_header_source()
+end
+
+vim.api.nvim_create_user_command("SwitchHeaderSource", function()
+  local current_file = vim.api.nvim_buf_get_name(0)
+
+  local file_name = vim.fn.expand("%:t")
+  local file_extension = vim.fn.expand("%:e")
+
+  local file_name_without_extension = vim.fn.expand("%:t:r")
+
+  local header_extensions = { "h", "hpp", "hh", "hxx" }
+  local source_extensions = { "c", "cpp", "cc", "cxx" }
+
+  local conversion = {
+    h   = "c",
+    hh  = "cc",
+    hpp = "cpp",
+    hxx = "cxx",
+
+    c   = "h",
+    cc  = "hh",
+    cpp = "hpp",
+    cxx = "hxx",
+  }
+
+  local new_extension = conversion[file_extension]
+
+  if new_extension == nil then
+    vim.notify("Cannot find the opposite extension for: " .. file_extension)
+    return
+  end
+
+  local new_file_name = file_name_without_extension .. "." .. new_extension
+  local new_file_path = vim.fn.expand("%:p:h") .. "/" .. new_file_name
+  local new_file_exists = vim.fn.filereadable(new_file_path) == 1
+
+
+  for _, buffer in pairs(vim.api.nvim_list_bufs()) do
+    local buffer_name = vim.api.nvim_buf_get_name(buffer)
+
+    if buffer_name == new_file_name then
+      is_open = true
+      break
+    end
+  end
+
+
+  -- if new_file_exists then
+  --
+  -- else
+  --   vim.notify("Cannot find file: " .. new_file_name)
+  -- end
+  --
+
+
+
+end, {})
+
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*.cpp", "*.hpp", "*.c", "*.h", "*.cc", "*.hh", "*.cxx", "*.hxx" },
+  callback = function(opts)
+    local buffer = opts.buf
+    local file_name = opts.file
+
+    local full_path = vim.api.nvim_buf_get_name(buffer)
+
+    vim.api.nvim_buf_set_keymap(buffer, "n",
+      "<leader>ls", "<cmd>SwitchHeaderSource<cr>",
+      { noremap = true, silent = true }
+    )
+
+    Events.await_event {
+      actor = "which-key",
+      event = "configured",
+      callback = function()
+        Safe.import_then('which-key', function(which_key)
+          which_key.register(
+            { "Switch Header/Source" },
+            { prefix = "<leader>ls", mode = "n", buffer = buffer }
+          )
+        end)
+      end
+    }
+  end
+})
