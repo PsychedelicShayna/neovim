f5dd838eed74fdba9c7ad2e93687c2a88c36c541
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2024-10-10 20:57:47 -0300

    Updated init to load new modules and get rid of old ones
    I'm going to make it autoload eventually.. eventually

--- lua/04-custom/init.lua
+++ lua/04-custom/init.lua
@@ -1,81 +1,84 @@
 local M = {}
 
 local function export(export_table)
-  local exported = {}
+    local exported = {}
 
-  for module_name, module_path in pairs(export_table) do
-    if type(module_name) == 'string' and type(module_path) == 'string' then
-      local module_ok, module = pcall(require, module_path)
+    for module_name, module_path in pairs(export_table) do
+        if type(module_name) == 'string' and type(module_path) == 'string' then
+            local module_ok, module = pcall(require, module_path)
 
-      if not module_ok then
-        PrintDbg('Failed to export module "' .. module_path .. '"', LL_ERROR, { module_ok, module })
-      else
-        exported[module_name] = module
-      end
+            if not module_ok then
+                PrintDbg('Failed to export module "' .. module_path .. '"', LL_ERROR, { module_ok, module })
+            else
+                exported[module_name] = module
+            end
+        end
     end
-  end
 
-  return exported
+    return exported
 end
 
 local lib_modules = {
-  check_greeter_skip = '04-custom.01-lib.check-greeter-skip',
-  functional         = '04-custom.01-lib.functional',
-  shell_precedence   = '04-custom.01-lib.shell-precedence',
-  smart_key_mapper   = '04-custom.01-lib.keymap_nx',
-  typecheck          = '04-custom.01-lib.typecheck',
-  window_sizer       = '04-custom.01-lib.window-sizer',
-  error_handler      = '04-custom.01-lib.error-handler',
-  map_key            = '04-custom.01-lib.mapkey',
+    check_greeter_skip = '04-custom.01-lib.check-greeter-skip',
+    functional         = '04-custom.01-lib.functional',
+    shell_precedence   = '04-custom.01-lib.shell-precedence',
+    typecheck          = '04-custom.01-lib.typecheck',
+    window_sizer       = '04-custom.01-lib.window-sizer',
+    error_handler      = '04-custom.01-lib.error-handler',
+    map_key            = '04-custom.01-lib.mapkey',
+    string_processing  = '04-custom.01-lib.string_processing',
 }
 
 UsrLib = export({
-  check_greeter_skip = lib_modules.check_greeter_skip,
-  map_key = lib_modules.map_key,
-  error_handler = lib_modules.error_handler
+    check_greeter_skip = lib_modules.check_greeter_skip,
+    map_key            = lib_modules.map_key,
+    error_handler      = lib_modules.error_handler,
+    string_processing  = lib_modules.string_processing,
 })
 
 
 local functionality_modules = {
-  background_control   = '04-custom.02-fun.background-control',
-  autocomment          = "04-custom.02-fun.autocomment",
-  auto_diag_loclist    = "04-custom.02-fun.auto-diag-loclist",
-  autorunner           = "04-custom.02-fun.autorunner",
-  cdtracker            = "04-custom.02-fun.cdtracker",
-  lsp_auto_format      = "04-custom.02-fun.lsp-auto-format",
-  spellcheck           = "04-custom.02-fun.spellcheck",
-  yank_highlighter     = "04-custom.02-fun.yank-highlighter",
-  cursorhold_highlight = "04-custom.02-fun.cursorhold-highlight",
-  scratch_pad          = "04-custom.02-fun.scratch-pad",
-  toggle_diagnostics   = "04-custom.02-fun.toggle-diagnostics",
-  switch_header_source = "04-custom.02-fun.switch_header_source",
-  flashbang            = "04-custom.02-fun.flashbang",
-  cat_on_fence         = "04-custom.02-fun.cat_on_fence"
+    background_control   = '04-custom.02-fun.background-control',
+    autocomment          = "04-custom.02-fun.autocomment",
+    auto_diag_loclist    = "04-custom.02-fun.auto-diag-loclist",
+    autorunner           = "04-custom.02-fun.autorunner",
+    cdtracker            = "04-custom.02-fun.cdtracker",
+    lsp_auto_format      = "04-custom.02-fun.lsp-auto-format",
+    spellcheck           = "04-custom.02-fun.spellcheck",
+    yank_highlighter     = "04-custom.02-fun.yank-highlighter",
+    cursorhold_highlight = "04-custom.02-fun.cursorhold-highlight",
+    scratch_pad          = "04-custom.02-fun.scratch-pad",
+    toggle_diagnostics   = "04-custom.02-fun.toggle-diagnostics",
+    switch_header_source = "04-custom.02-fun.switch_header_source",
+    flashbang            = "04-custom.02-fun.flashbang",
+    cat_on_fence         = "04-custom.02-fun.cat_on_fence",
+    zoxidecd             = "04-custom.02-fun.zoxidecd"
 }
 
 UsrFun = functionality_modules
 
 local auto_load_functionality = {
-  -- UsrFun.lsp_auto_format,
-  UsrFun.cat_on_fence,
-  UsrFun.yank_highlighter,
-  UsrFun.auto_diag_loclist,
-  UsrFun.background_control,
-  UsrFun.scratch_pad,
-  UsrFun.spellcheck,
-  UsrFun.autorunner,
-  UsrFun.toggle_diagnostics,
-  UsrFun.switch_header_source,
-  UsrFun.flashbang,
+    -- UsrFun.lsp_auto_format,
+    UsrFun.cat_on_fence,
+    UsrFun.yank_highlighter,
+    UsrFun.auto_diag_loclist,
+    UsrFun.background_control,
+    UsrFun.scratch_pad,
+    UsrFun.spellcheck,
+    UsrFun.autorunner,
+    UsrFun.toggle_diagnostics,
+    UsrFun.switch_header_source,
+    UsrFun.flashbang,
+    UsrFun.zoxidecd,
 }
 
 for name, path in pairs(auto_load_functionality) do
-  local module_ok, module = pcall(require, path)
+    local module_ok, module = pcall(require, path)
 
-  if not module_ok then
-    PrintDbg('Failed to autoload module "' .. name
-      .. '" using path "' .. vim.inspect(path) .. '"', LL_ERROR, { module_ok, module })
-  end
+    if not module_ok then
+        PrintDbg('Failed to autoload module "' .. name
+            .. '" using path "' .. vim.inspect(path) .. '"', LL_ERROR, { module_ok, module })
+    end
 end
 
 -- require 'lib.lsp_auto_format'
