424d87b4c805ecbd5be7649991558b8d473d3583
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2024-10-21 00:24:44 -0300

    Added tcd and lcd equivalents for Zoxide

--- lua/03-usermade/02-commands/01-zoxide-cd.lua
+++ lua/03-usermade/02-commands/01-zoxide-cd.lua
@@ -12,108 +12,124 @@ local M = {}
 
 ---@return boolean
 function M.is_directory(path)
-    local stat = vim.loop.fs_stat(path)
-    return stat and stat.type == "directory" or false
+  local stat = vim.loop.fs_stat(path)
+  return stat and stat.type == "directory" or false
 end
 
 ---@param query string
 function M.get_completion_list(query)
+  ---@type string
+  local command_template = "zoxide query --list --exclude %q %q"
+
+  ---@type string
+  local command = string.format(command_template, vim.fn.getcwd(), query)
+
+  ---@type file*?, string?
+  local handle = io.popen(command, "r")
+
+  if handle == nil then
+    return {}
+  end
+
+  ---@type string
+  local results = handle:read("*a")
+  handle:close()
+
+  ---@type string[]
+  local lines = {}
+
+  for line in results:gmatch("[^\r\n]+") do
     ---@type string
-    local command_template = "zoxide query --list --exclude %q %q"
+    line = line:match("^%s*(.-)%s*$")
+    table.insert(lines, line)
+  end
 
-    ---@type string
-    local command = string.format(command_template, vim.fn.getcwd(), query)
-
-    ---@type file*?, string?
-    local handle = io.popen(command, "r")
-
-    if handle == nil then
-        return {}
-    end
-
-    ---@type string
-    local results = handle:read("*a")
-    handle:close()
-
-    ---@type string[]
-    local lines = {}
-
-    for line in results:gmatch("[^\r\n]+") do
-        ---@type string
-        line = line:match("^%s*(.-)%s*$")
-        table.insert(lines, line)
-    end
-
-    return lines
+  return lines
 end
 
 ---@param opts table
 ---@return boolean Whether or not the directory completed via Zoxide.
-function M.zoxide_cd(opts)
-    if type(opts) ~= 'table' then
-        PrintDbg(string.format("zoxide_cd received a %q as its input, only receive an options table", type(opts)),
-            LL_ERROR)
+function M.zoxide_cd(opts, cdcmd)
+  if type(opts) ~= 'table' then
+    PrintDbg(string.format("zoxide_cd received a %q as its input, only receive an options table", type(opts)),
+      LL_ERROR)
+  end
+
+  if not cdcmd then
+    cdcmd = "cd"
+  end
+
+  ---@type string
+  local path = opts.args
+
+  ---@type string
+  local zpath = ""
+
+  ---@type file*?, string?
+  local handle = io.popen("zoxide query " .. path, "r")
+  if handle ~= nil then
+    zpath = handle:read("*a")
+
+    if type(zpath) == 'string' then
+      zpath = zpath:match("^%s*(.-)%s*$")
     end
 
-    ---@type string
-    local path = opts.args
+    handle:close()
+  end
 
-    ---@type string
-    local zpath = ""
+  local bad_result = (zpath == nil or zpath == "" or type(zpath) ~= 'string' or zpath == "zoxide: no match found")
+  local path_exists = M.is_directory(path)
+  local destination = zpath
 
-    ---@type file*?, string?
-    local handle = io.popen("zoxide query " .. path, "r")
+  -- The path does exist but Zoxide isn't aware, we should inform it.
+  if bad_result and path_exists then
+    handle = io.popen(string.format("zoxide add %s", path))
+    destination = path
+
     if handle ~= nil then
-        zpath = handle:read("*a")
-
-        if type(zpath) == 'string' then
-            zpath = zpath:match("^%s*(.-)%s*$")
-        end
-
-        handle:close()
+      handle:close()
+    else
+      PrintDbg(string.format("Could not inform Zoxide that %q exists; handle was nil."), LL_WARN)
     end
+  elseif not M.is_directory(zpath) then
+    vim.notify(string.format("Zoxide gave back a path that does not exist: %q", zpath))
+    return false
+  end
 
-    local bad_result = (zpath == nil or zpath == "" or type(zpath) ~= 'string' or zpath == "zoxide: no match found")
-    local path_exists = M.is_directory(path)
-    local destination = zpath
+  vim.cmd(string.format("%s %s", cdcmd, destination))
+  vim.cmd("pwd")
 
-    -- The path does exist but Zoxide isn't aware, we should inform it.
-    if bad_result and path_exists then
-        handle = io.popen(string.format("zoxide add %s", path))
-        destination = path
+  return not bad_result
+end
 
-        if handle ~= nil then
-            handle:close()
-        else
-            PrintDbg(string.format("Could not inform Zoxide that %q exists; handle was nil."), LL_WARN)
-        end
-    elseif not M.is_directory(zpath) then
-        vim.notify(string.format("Zoxide gave back a path that does not exist: %q", zpath))
-        return false
-    end
-
-    vim.cmd(string.format("cd %s", destination))
-    vim.cmd("pwd")
-
-    return not bad_result
+local function zoxide_complete(_arglead, cmdline, _cursorpos)
+  ---@type string?
+  local args = cmdline:match("^Z (.+)")
+  return (args and M.get_completion_list(args)) or {}
 end
 
 vim.api.nvim_create_user_command("Z",
-    function(query)
-        M.zoxide_cd(query)
-    end,
-    {
-        nargs = '?',
-        range = '%',
-        complete = function(_arglead, cmdline, _cursorpos)
-            ---@type string?
-            local args = cmdline:match("^Z (.+)")
-            return (args and M.get_completion_list(args)) or {}
-        end,
-        preview = function()
-            return "Test"
-        end
-    }
+  function(query) M.zoxide_cd(query) end,
+  { nargs = '?', range = '%', complete = zoxide_complete, }
 )
+
+vim.api.nvim_create_user_command("Zt",
+  function(query) M.zoxide_cd(query, "tcd") end,
+  { nargs = '?', range = '%', complete = zoxide_complete, }
+)
+
+vim.api.nvim_create_user_command("Zl",
+  function(query) M.zoxide_cd(query, "lcd") end,
+  { nargs = '?', range = '%', complete = zoxide_complete, }
+)
+
+-- vim.api.nvim_create_user_command("Zt",
+--   function(query) M.zoxide_cd(query, "t") end,
+--   { nargs = '?', range = '%', complete = zoxide_complete, })
+--
+-- vim.api.nvim_create_user_command("Zl",
+--   function(query) M.zoxide_cd(query, "l") end,
+--   { nargs = '?', range = '%', complete = zoxide_complete, })
+--
 
 return M
