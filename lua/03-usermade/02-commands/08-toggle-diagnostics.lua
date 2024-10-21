8a1810bc736880c96a495a3cfc256d7ec91ca0ef
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2024-10-21 00:24:59 -0300

    Fixed ToggleDiagnostics command

--- lua/03-usermade/02-commands/08-toggle-diagnostics.lua
+++ lua/03-usermade/02-commands/08-toggle-diagnostics.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_user_command("ToggleDiagnostics", function()
   if vim.diagnostic.is_enabled() then
-    vim.diagnostic.enable()
-  else
     vim.diagnostic.enable(false)
+  else
+    vim.diagnostic.enable(true)
   end
 
   vim.notify("Diagnostics turned " .. ((vim.diagnostic.is_enabled() and "on") or "off"))
