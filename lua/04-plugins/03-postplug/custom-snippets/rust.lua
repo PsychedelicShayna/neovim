3b8258b69af24ebe2a82449b94495ba1aed91fb8
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2024-11-18 01:52:50 -0300

    Added more Rust snippets

--- lua/04-plugins/03-postplug/custom-snippets/rust.lua
+++ lua/04-plugins/03-postplug/custom-snippets/rust.lua
@@ -5,11 +5,17 @@ Safe.import_then("luasnip", function(ls)
   local fmt = require("luasnip.extras.fmt").fmt
 
   ls.add_snippets("rust", {
-    s("Op", fmt("Option<{}>", { i(0) })),
-    s("So", fmt("Some<{}>", { i(0) })),
-    s("Ok", fmt("Ok<{}>", { i(0) })),
+    s("op", fmt("Option<{}>", { i(0) })),
+    s("so", fmt("Some<{}>", { i(0) })),
+    s("ok", fmt("Ok<{}>", { i(0) })),
     s("pm", fmt("pub mod {};", { i(0) })),
     s("pdb", fmt("eprintln!(\"{{:?}}\", {});", { i(0) })),
     s("ctkc", fmt("KeyCode::Char('{}')", { i(0) })),
+    s("ahf", fmt("fn {} ({}) -> ah::Result<()> {{\n{}\n\n    Ok(())\n}}", { i(1),i(2),i(3) })),
+    s("ahim", fmt("fn {} (&self, {}) -> ah::Result<()> {{\n{}\n\n    Ok(())\n}}", { i(1),i(2),i(3) })),
+    s("ahmm", fmt("fn {} (&mut self, {}) -> ah::Result<()> {{\n{}\n\n    Ok(())\n}}", { i(1),i(2),i(3) })),
+    s("ahr", fmt("-> ah::Result<({})> {{\n{}\n)}}", { i(0), i(2) })),
+    s("ahb", fmt("ah::bail!(\"{}\",);", { i(0) })),
+    s("der", fmt("#[derive(Debug, Clone, Default{})]", { i(0) })),
   })
 end)
