2b2c69fc6765a52e13b9f92a899c8f54652a4cb1
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2025-01-06 21:17:05 -0300

    Added more WhichKey groups.

--- lua/07-keymaps/00-wk-groups.lua
+++ lua/07-keymaps/00-wk-groups.lua
@@ -5,14 +5,20 @@ Events.await_event {
   callback = function()
     Safe.import_then("which-key", function(wk)
       wk.add {
-        { "<space>b", group = "[Buffer]" },
-        { "<space>c", group = "[Configure]" },
-        { "<space>d", group = "[DAP]" },
-        { "<space>f", group = "[Find]" },
-        { "<space>l", group = "[LSP]" },
-        { "<space>w", group = "[Window]" },
-        { "<space>d", group = "[Debugging]" },
-          { "<leader>db",  group = "[Breakpoints]" },
+        { "<space>b",   group = "[Buffer]" },
+        { "<space>c",   group = "[Configure]" },
+        { "<space>d",   group = "[DAP]" },
+        { "<space>f",   group = "[Find]" },
+        { "<space>l",   group = "[LSP]" },
+        { "<space>w",   group = "[Window]" },
+        { "<space>d",   group = "[Debugging]" },
+        { "<leader>db", group = "[Breakpoints]" },
+        { "<leader>ds", group = "[Session]" },
+        { "<leader>du", group = "[DAP UI]" },
+        { "<leader>j",  group = "[Jump]" },
+        { "<leader>jk",  group = "[Prevous]" },
+        { "<leader>jj",  group = "[Next]" },
+        { "<leader>m",  group = "[NaviMarks]" },
       }
     end)
   end
