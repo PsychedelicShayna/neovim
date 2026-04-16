59551c594522c7a26265440a4e5ce832c5929c48
PsychedelicShayna <34726837+PsychedelicShayna@users.noreply.github.com>
2026-04-16 17:24:14 -0300

    Nice UI components by Folke! Unlike his Noice, these are practical!
    Even the scrolling is practical if you just make it a bit faster,
    because you know in what direction you just jumped, your eyes catch the
    movement for a second
    
    Still need to configure it though.
    
    (Also holy shit the amount of time I spent looking at all the equations
    for the easing method that are undocumented but so cool!)
    
    Protip: out > inOut > in
    
    Unless it's circ, then.. well then you'll love it a lot and then hate it
    when you shrink your terminal.

---
+++ lua/04-plugins/lazy-spec/snacks.lua
@@ -0,0 +1,37 @@
+return {
+  'folke/snacks.nvim',
+  lazy = false,
+  priority = 1001,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    animate = { enabled = true },
+    scroll = {
+      enabled = true,
+      animate = {
+        duration = { step = 16, total = 96 },
+        easing = "outSine",
+      },
+      -- faster animation when repeating scroll after delay
+      animate_repeat = {
+        delay = 100,     -- delay in ms before using the repeat animation
+        duration = { step = 16, total = 64 },
+        easing = "outCubic",
+      },
+      -- what buffers to animate
+      filter = function(buf)
+        return vim.g.snacks_scroll ~= false and vim.b[buf].snacks_scroll ~= false and
+            vim.bo[buf].buftype ~= "terminal"
+      end,
+    },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  }
+}
